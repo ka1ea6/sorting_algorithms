955f9026f9f27699480508d2c7ab3fc827075d9b
ka1ea6 <kaleab77gulilat@gmail.com>
2022-11-05 06:09:35 -0400

    insertion init

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,59 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Function to sort a doubly linked list
+ * containting integer using insertion sort algorithm.
+ *
+ * @list: list to be sorted.
+ * Return: Nothing.
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp, *curr, *sorted_iter;
+
+	if (!list || !(*list)->next)
+		return;
+
+	curr = (*list)->next;
+
+	while (curr)
+	{
+		sorted_iter = curr->prev;
+		temp = curr->next;
+
+		if (curr->n > sorted_iter->n)
+		{
+			curr = curr->next;
+			continue;
+		}
+
+		while(sorted_iter->n > curr->n)
+		{
+			sorted_iter->next = curr->next;
+			if (curr->next)
+				curr->next->prev=  sorted_iter;
+			curr->next = sorted_iter;
+			if (sorted_iter->prev)
+			{
+				sorted_iter->prev->next = curr;
+			}
+			else
+			{
+				*list = curr;
+			}
+
+			curr->prev = sorted_iter->prev;
+			sorted_iter->prev = curr;
+			print_list(*list);
+			if (!(sorted_iter->prev->prev))
+				break;
+
+			sorted_iter = sorted_iter->prev->prev;
+		}
+
+		curr = temp;
+	}
+}
+
+
