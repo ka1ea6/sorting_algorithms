0dbccf92906f95c6834c7cca0086c5e016fdf1b0
ka1ea6 <kaleab77gulilat@gmail.com>
2022-11-06 01:36:16 -0400

    selection correction;

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,9 +14,6 @@ void selection_sort(int *array, size_t size)
 {
 	int temp, i, j, min_index;
 	
-	if (check_array_is_sorted(array, size))
-		return;
-
 	for (i = 0; i < (int)size; i++)
 	{
 		min_index = i;
